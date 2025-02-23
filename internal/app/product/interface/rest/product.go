d4f9b0120ef5cbfa413f0027398d2cedf360a918
ayas <ahmdyaasiin@student.ub.ac.id>
2025-02-23 11:39:11 +0700

    add product handler

---
+++ internal/app/product/interface/rest/product.go
@@ -0,0 +1,123 @@
+package rest
+
+import (
+	"github.com/ahmdyaasiin/workshop-intern-be-2025/internal/app/product/usecase"
+	"github.com/ahmdyaasiin/workshop-intern-be-2025/internal/domain/dto"
+	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"net/http"
+)
+
+type ProductHandler struct {
+	Validator      *validator.Validate
+	ProductUseCase usecase.ProductUsecaseItf
+}
+
+func NewProductHandler(routerGroup fiber.Router, _validator *validator.Validate, productUseCase usecase.ProductUsecaseItf) {
+	productHandler := ProductHandler{
+		Validator:      _validator,
+		ProductUseCase: productUseCase,
+	}
+
+	routerGroup = routerGroup.Group("/products")
+
+	routerGroup.Get("/", productHandler.GetAllProducts)
+	routerGroup.Post("/", productHandler.CreateProduct)
+	routerGroup.Get("/:id", productHandler.GetSpecificProduct)
+	routerGroup.Patch("/:id", productHandler.UpdateProduct)
+	routerGroup.Delete("/:id", productHandler.DeleteProduct)
+}
+
+func (h *ProductHandler) CreateProduct(ctx *fiber.Ctx) error {
+	var request dto.RequestCreateProduct
+	if err := ctx.BodyParser(request); err != nil {
+		return err
+	}
+
+	if err := h.Validator.Struct(request); err != nil {
+		return err
+	}
+
+	res, err := h.ProductUseCase.CreateProduct(request)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(http.StatusCreated).JSON(fiber.Map{
+		"message": "success create product",
+		"payload": res,
+	})
+}
+
+func (h *ProductHandler) GetAllProducts(ctx *fiber.Ctx) error {
+
+	res, err := h.ProductUseCase.GetAllProducts()
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(http.StatusOK).JSON(fiber.Map{
+		"message": "success get products",
+		"payload": res,
+	})
+}
+
+func (h *ProductHandler) GetSpecificProduct(ctx *fiber.Ctx) error {
+	productID, err := uuid.Parse(ctx.Params("id"))
+	if err != nil {
+		return err
+	}
+
+	res, err := h.ProductUseCase.GetSpecificProduct(productID)
+	if err != nil {
+		return err
+	}
+
+	return ctx.Status(http.StatusOK).JSON(fiber.Map{
+		"message": "success get specific product",
+		"payload": res,
+	})
+}
+
+func (h *ProductHandler) UpdateProduct(ctx *fiber.Ctx) error {
+	var request dto.RequestUpdateProduct
+
+	err := ctx.BodyParser(request)
+	if err != nil {
+		return err
+	}
+
+	productID, err := uuid.Parse(ctx.Params("id"))
+	if err != nil {
+		return err
+	}
+
+	err = h.Validator.Struct(request)
+	if err != nil {
+		return err
+	}
+
+	request.ID = productID
+
+	err = h.ProductUseCase.UpdateProduct(request)
+	if err != nil {
+		return err
+	}
+
+	return ctx.SendStatus(http.StatusNoContent)
+}
+
+func (h *ProductHandler) DeleteProduct(ctx *fiber.Ctx) error {
+	productID, err := uuid.Parse(ctx.Params("id"))
+	if err != nil {
+		return err
+	}
+
+	err = h.ProductUseCase.DeleteProduct(productID)
+	if err != nil {
+		return err
+	}
+
+	return ctx.SendStatus(fiber.StatusNoContent)
+}
