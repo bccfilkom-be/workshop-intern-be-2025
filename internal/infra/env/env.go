f630163ca4aaf558737b694598000a0c4defc0cc
ayas <ahmdyaasiin@student.ub.ac.id>
2025-02-23 11:39:59 +0700

    add load env for infrastructure application

---
+++ internal/infra/env/env.go
@@ -0,0 +1,34 @@
+package env
+
+import (
+	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
+)
+
+type Env struct {
+	AppName string `env:"APP_NAME,required"`
+	AppPort int    `env:"APP_PORT,required"`
+
+	DatabaseHost     string `env:"DB_HOST,required"`
+	DatabasePort     int    `env:"DB_PORT,required"`
+	DatabaseUsername string `env:"DB_USERNAME,required"`
+	DatabasePassword string `env:"DB_PASSWORD,required"`
+	DatabaseName     string `env:"DB_NAME,required"`
+}
+
+func New() (*Env, error) {
+
+	err := godotenv.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	config := new(Env)
+
+	err = env.Parse(config)
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
