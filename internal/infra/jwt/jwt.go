69018b17bce593dbe40b9abdd30ed593072b2910
Naufal Haris <naufal.michiels@gmail.com>
2025-02-23 12:33:17 +0700

    feat(jwt): add validation

--- internal/infra/jwt/jwt.go
+++ internal/infra/jwt/jwt.go
@@ -17,12 +17,20 @@ func NewJWT() JWTI {
 	return &JWT{}
 }
 
-func (j *JWT) GenerateToken(userId uuid.UUID) (string, error) {
+type Claims struct {
+	UserId uuid.UUID
+	jwt.RegisteredClaims
+}
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"userID": userId,
-		"exp":    time.Now().Add(time.Hour * 2).Unix(),
-	})
+func (j *JWT) GenerateToken(userId uuid.UUID) (string, error) {
+	claim := Claims{
+		UserId: userId,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 2)),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
 
 	tokenString, err := token.SignedString([]byte("secret"))
 	if err != nil {
@@ -30,4 +38,25 @@ func (j *JWT) GenerateToken(userId uuid.UUID) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func (j *JWT) ValidateToken(tokenString string) (uuid.UUID, error) {
+	var claim Claims
+	var id uuid.UUID
+
+	token, err := jwt.ParseWithClaims(tokenString, claim, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+
+	if err != nil {
+		return id, err
+	}
+
+	if !token.Valid {
+		return id, err
+	}
+
+	id = claim.UserId
+	
+	return id, nil
 }
